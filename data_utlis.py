Fixes issues with gradients

data_utlis.py
@@ -126,7 +126,7 @@ def pad_and_embed(data):
     embed = Embedding(len(vocab), len(vocab))
 
     seq_lengths = LongTensor(list(map(len, vectorized_seqs)))
-    seq_tensor = torch.zeros((len(vectorized_seqs), seq_lengths.max())).long()
+    seq_tensor = torch.zeros((len(vectorized_seqs), seq_lengths.max()), requires_grad=True).long()
     extended_pssm = np.zeros((len(vectorized_seqs), seq_lengths.max(), pssms[0].shape[1]))
     extended_coords = np.zeros((len(vectorized_seqs), seq_lengths.max()*3, 3))
     extended_mask = np.zeros((len(vectorized_seqs), seq_lengths.max()*3))
@@ -167,8 +167,8 @@ class ProteinNetDataset(Dataset):
         seq_pssm = np.concatenate([sequence, pssm], axis=1)
 
         sample = {'name': name,
-                  'sequence': torch.tensor(seq_pssm),
-                  'coords': torch.tensor(coords),
+                  'sequence': torch.tensor(seq_pssm, requires_grad=True),
+                  'coords': torch.tensor(coords, requires_grad=True),
                   'length': length,
                   'mask': mask
                   }

geometric_ops.py
@@ -58,13 +58,13 @@ def calculate_dihedrals(p, alphabet):
 
 def drmsd(u, v, mask=None):
     #type: (torch.Tensor, torch.Tensor, torch.Tensor) -> (torch.Tensor)
-    diffs = torch.zeros(0).move_to_gpu()
+    diffs = torch.zeros(0, requires_grad=True).move_to_gpu()
     L = u.shape[0]/3
     for batch in range(u.shape[1]):
         u_b, v_b = u[:, batch], v[:, batch]
         mask_b = mask[:, batch]
         diff = calculate_pairwise_distances(u_b, v_b, mask_b)
-        diffs = torch.cat([diffs, torch.tensor([diff.norm()/(L*(L-1))], requires_grad=True).move_to_gpu()])
+        diffs = torch.cat([diffs, diff.norm(dim=0, keepdim=True)/(L*(L-1))])
     norm = diffs.norm(dim=0)
     return diffs
 
@@ -79,12 +79,12 @@ def calculate_pairwise_distances(u, v, mask=None):
     Returns:
         [3L,1] with diagonal elements 0
     """
-    diffs = torch.zeros(0).move_to_gpu()
+    diffs = torch.zeros(0, requires_grad=True).move_to_gpu()
     for atom_id in range(u.shape[0]):
         if mask is not None and mask[atom_id] == 0:
             continue
-        diff = ((u-u[atom_id]).norm(dim=1)-(v-v[atom_id]).float().norm(dim=1)).norm()  # [3L, 1]
-        diffs = torch.cat([diffs, torch.tensor([diff], requires_grad=True).move_to_gpu()])
+        diff = ((u-u[atom_id]).norm(dim=1)-(v-v[atom_id]).float().norm(dim=1)).norm(keepdim=True, dim=0)  # [3L, 1]
+        diffs = torch.cat([diffs, diff])
     return diffs
 
 

model.py
@@ -46,7 +46,6 @@ class dRMSD(nn.Module):
 class RGN(nn.Module):
     def __init__(self, d_in, linear_out=20, h=800, num_layers=2, alphabet_size=20):
         super(RGN, self).__init__()
-        self.lstm_layers = []
         self.num_layers = num_layers
         self.hidden_size = h
 
@@ -54,19 +53,14 @@ class RGN(nn.Module):
 
         self.error = dRMSD()
 
-        self.model = nn.Sequential(OrderedDict([
-            ('LSTM1', nn.LSTM(d_in, hidden_size=h, bidirectional=True)),
-            ('LSTM2', nn.LSTM(h * 2, hidden_size=h, bidirectional=True)),
-        ]))
-
         self.lstm = nn.LSTM(d_in, h, num_layers, bidirectional=True)
 
     def forward(self, x):
         lens = list(map(len, x))
         batch_sz = len(lens)
         x = x.float().transpose(0, 1).contiguous()
-        h0 = torch.zeros((self.num_layers * 2, batch_sz, self.hidden_size)).move_to_gpu()
-        c0 = torch.zeros((self.num_layers * 2, batch_sz, self.hidden_size)).move_to_gpu()
+        h0 = torch.zeros((self.num_layers * 2, batch_sz, self.hidden_size)).move_to_gpu().requires_grad_(True)
+        c0 = torch.zeros((self.num_layers * 2, batch_sz, self.hidden_size)).move_to_gpu().requires_grad_(True)
 
         lstm_out, _ = self.lstm(x, (h0, c0))
 
@@ -93,10 +87,11 @@ class RGN(nn.Module):
                 l = loss.sum()/batch_size
                 l.backward()
                 optimizer.step()
+                print(list(map(lambda x: x.grad, self.parameters())))
                 if batch_idx % log_interval == 0:
                     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                         epoch, batch_idx * len(data), len(train_loader.dataset),
-                               100. * batch_idx / len(train_loader), l))
+                               100. * batch_idx / len(train_loader), l.item()))
                 # torch.cuda.empty_cache()
 
     def _transform_for_lstm(self, data):