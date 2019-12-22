3c67ada5db8170dc777b11350ed2310c20a08deb
donikv <donikv@gmail.com>
2019-12-23 00:16:56 +0100

    Adds model saving to test

--- model_test.py
+++ model_test.py
@@ -12,7 +12,7 @@ from pathlib import Path
 from model import RGN
 import sys
 from gpu_profile import gpu_profile
-
+import torch
 sys.settrace(gpu_profile)
 home = str(Path.home())
 # pn_path = home + '/casp7/training_30'
@@ -22,4 +22,6 @@ model = RGN(42)
 model.cuda(0)
 model.train(pn_path, log_interval=1, optimiz='Adam', epochs=1, profile_gpu=True)
 model.test(pn_test)
+f = open("model.pickle", "w+")
+torch.save(model, f)
 exit()
